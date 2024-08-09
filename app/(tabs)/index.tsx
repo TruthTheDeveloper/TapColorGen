94f54f470762e2d00747df30cc75fbb63709e4c3
TruthTheDeveloper <henrysempire111@gmail.com>
2024-08-09 17:34:42 +0100

    added styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,38 @@
-import { StyleSheet, View , Text, SafeAreaView} from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { useState } from 'react';
+import { StyleSheet, View , Text, SafeAreaView, TouchableWithoutFeedback} from 'react-native';
 
 export default function HomeScreen() {
-  return (
+
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
+  const [tapCount, setTapCount] = useState(0);
+
+  const handleScreenTap = () => {
+
+  }
+  
+
+
+
    <SafeAreaView>
-    <View>
-      <Text>Hello there</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleScreenTap}>
+    <View style={[styles.container, { backgroundColor }]}>
+        <ThemedText type='title'>Hello there</ThemedText>
+        <ThemedText style={styles.colorInfo}>Current Color: {backgroundColor}</ThemedText>
+      </View>
+    </TouchableWithoutFeedback>
    </SafeAreaView>
-  );
+  
 }
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  colorInfo: {
+    fontSize: 16,
+  },
 });
