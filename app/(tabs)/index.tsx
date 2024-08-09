bbc97f1f537fa3d36e681aac8d41c9bf8866a3fa
TruthTheDeveloper <henrysempire111@gmail.com>
2024-08-09 17:43:35 +0100

    added a color generator logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,27 +5,41 @@ import { StyleSheet, View , Text, SafeAreaView, TouchableWithoutFeedback} from '
 export default function HomeScreen() {
 
   const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
-  const [tapCount, setTapCount] = useState(0);
+  
+  const generateRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  };
 
   const handleScreenTap = () => {
+    const newColor = generateRandomColor();
+    setBackgroundColor(newColor);
+
 
   }
   
 
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <TouchableWithoutFeedback onPress={handleScreenTap}>
+        <View style={[styles.container, { backgroundColor }]}>
+          <ThemedText type='title'>Hello there</ThemedText>
+          <ThemedText style={styles.colorInfo}>Current Color: {backgroundColor}</ThemedText>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  )
 
-
-   <SafeAreaView>
-    <TouchableWithoutFeedback onPress={handleScreenTap}>
-    <View style={[styles.container, { backgroundColor }]}>
-        <ThemedText type='title'>Hello there</ThemedText>
-        <ThemedText style={styles.colorInfo}>Current Color: {backgroundColor}</ThemedText>
-      </View>
-    </TouchableWithoutFeedback>
-   </SafeAreaView>
+   
   
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
